34aedfc4e3bca15925d150f44c5966f8f8d54db6
vaniakazeko <vania.kazeko.00@bk.ru>
2022-12-14 16:55:55 +0300

    add login and registeration

--- users/views.py
+++ users/views.py
@@ -1,10 +1,23 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
 
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
 
     contex = {
         'form': form,
@@ -13,4 +26,19 @@ def login(request):
 
 
 def registration(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
