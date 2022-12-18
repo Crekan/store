e5bfbe265655c24dbae82caf892e7b1e02eb4611
vaniakazeko <vania.kazeko.00@bk.ru>
2022-12-18 19:40:18 +0300

    add posgres

--- store/settings.py
+++ store/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "store_db",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
